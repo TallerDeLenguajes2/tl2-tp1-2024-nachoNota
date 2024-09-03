0bbe5c9aeaee74535ea2e83ae34d35ce489bd321
nachoNota <nachonota13@gmail.com>
2024-09-03 10:32:52 -0300

    Creo la clase AccesoADatos y trabajo en el metodo para leer archivos json

---
+++ AccesoADatos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+
+public class AccesoADatos
+{
+	public virtual string LeerArchivo(string rutaArchivo) { }
+}
+
+public class AccesoCSV : AccesoADatos
+{
+    public override string LeerArchivo(string rutaArchivo)
+    {
+
+        return ;
+    }
+}
+
+public class AccesoJson : AccesoADatos
+{
+    public override string LeerArchivo(string rutaArchivo)
+    {
+        string linea;
+        using (FileStream archivo = new FileStream(rutaArchivo, File.Open))
+        {
+            using (StreamReader st = new StreamReader(archivo))
+            {
+                linea = sr.ReadToEnd();
+                sr.Close();
+            }
+        }
+    |   return linea;
+    }
+}
